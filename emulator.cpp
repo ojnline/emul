0507b37f92ff34ec2eead8c817adc542a51f509b
ojnline <58391142+ojnline@users.noreply.github.com>
2019-12-17 23:42:45 +0100

    Made the emulator c89 compatible and generally cleaned it up.

--- emulator.cpp
+++ emulator.cpp
@@ -1,29 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <cstdlib>
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
-#include <cstdint>
-#include <algorithm>
+#include <stdlib.h>
+#include <stdint.h>
 
 #define RAM_SIZE 4096
 #define MEMORY_UNBLOCKED 0x0000
 #define MEMORY_BLOCKED   0x0001
 
-int main() {
+int main(int argc, char **argv) {
 
 	char memory[RAM_SIZE];
 	char memory_map[RAM_SIZE];
 
-	bool OK = true;
+	int OK = 1;
 
 	uint8_t A = 0;
-	char* PC = memory + 0x100;
-
-	char buf[2];
-
-	unsigned counter = 0;
-	unsigned counter_ = 0;
+	char* PC = memory + 0x100u;
 
 	memset(memory, 0, RAM_SIZE);
 	memset(memory_map, MEMORY_UNBLOCKED, RAM_SIZE);
@@ -32,90 +27,84 @@ int main() {
 	fseek(f, 0, SEEK_END);
 	size_t size = ftell(f);
 	fseek(f, 0, SEEK_SET);
-	fread(memory+0x100, 1, size, f);
+	assert(fread(memory + 0x100u, 1, size, f) > 0);
 	fclose(f);
 
-	while (OK) {
+	while (OK == 1) {
 
-		uint16_t instruction = (static_cast<uint16_t>(PC[1]) & 0x00FF) + (static_cast<uint16_t>(PC[0]) << 8);
-		uint8_t  XX = instruction & 0x00FF;
-		uint16_t XXX = instruction & 0x0FFF;
+		uint16_t instruction = ((uint16_t)(PC[1]) & 0x00FFu) + ((uint16_t)(PC[0]) << 8);
+		uint8_t  XX = instruction & 0x00FFu;
+		uint16_t XXX = instruction & 0x0FFFu;
 		char* _XXX_ = memory + XXX;
 		char* _XXX_map = memory_map + XXX;
 
-		//printf("\nOP:0x%04X A:0x%02X PC:0x%03x C:%u", instruction, A & 0xFF, PC-memory & 0xFFF, counter_);
+		/*printf("\nOP:0x%04X A:0x%02X PC:0x%03x C:%u", instruction, A & 0xFF, static_cast<unsigned>(PC-memory & 0xFFF), counter_);*/
 
-		if (counter == 150)
-			counter = 0;
-		if (counter_ == 3876)
-			int i = 0;
-		counter++;
-		counter_++;
-
-		switch (PC[0] & 0xF0)
+		switch (PC[0] & 0xF0u)
 		{
-		// -------------------------------
-		// ARITHMETIC
-		// -------------------------------
-		case 0x00:
-			switch (PC[0] & 0x0F)
+		
+		/* -------------------------------*/
+		/* ARITHMETIC                     */
+		/* -------------------------------*/
+		case 0x00u:
+			switch (PC[0] & 0x0Fu)
 			{
-			case 0x0:
+			case 0x0u:
 				A += XX;
 				goto next_instruction;
-			case 0x1:
+			case 0x1u:
 				A = XX;
 				goto next_instruction;
-			case 0x2:
+			case 0x2u:
 				A ^= XX;
 				goto next_instruction;
-			case 0x3:
+			case 0x3u:
 				A |= XX;
 				goto next_instruction;
-			case 0x4:
+			case 0x4u:
 				A &= XX;
 				goto next_instruction;
 			default:
 				goto invalid_instruction;
 			};
-		case 0x80:
+		case 0x80u:
 			A = *_XXX_;
 			goto next_instruction;
-		case 0xD0:
+		case 0xD0u:
 			if (*_XXX_map == MEMORY_UNBLOCKED)
 				*_XXX_ ^= A;
 			goto next_instruction;
-		case 0xF0:
+		case 0xF0u:
 			if (*_XXX_map == MEMORY_UNBLOCKED)
 				*_XXX_ = A;
 			goto next_instruction;
 
-		// -------------------------------
-		// CONTROL FLOW
-		// -------------------------------
-		case 0x20:
+		/* -------------------------------*/
+		/* FLOW CONTROL                   */
+		/* -------------------------------*/
+		case 0x20u:
 			PC = memory + XXX;
 			goto jump;
-		case 0x30:
+		case 0x30u:
 			if (A == 0x00) {
 				PC = _XXX_;
 				goto jump;
 			}
 			goto next_instruction;
-		case 0x40:
+		case 0x40u:
 			if (A == 0x01) {
 				PC = _XXX_;
 				goto jump;
 			}
 			goto next_instruction;
-		case 0x50:
+		case 0x50u:
 			if (A == 0xFF) {
 				PC = _XXX_;
 				goto jump;
 			}
 			goto next_instruction;
-		case 0x60:
-			if ((instruction & 0xFF00) != 0x6000) // TODO make not suck
+		case 0x60u:
+			if ((instruction & 0xFF00) != 0x6000)
 				goto invalid_instruction;
 			else if (A > XX) {
 				A = 0xFF;
@@ -128,8 +117,8 @@ int main() {
 			else
 				A = 0x00;
 			goto next_instruction;
-		case 0x70:
-			if (A > *_XXX_) {
+		case 0x70u:
+			if (A > * _XXX_) {
 				A = 0xFF;
 				goto next_instruction;
 			}
@@ -140,50 +129,52 @@ int main() {
 			else
 				A = 0x00;
 			goto next_instruction;
-		// -------------------------------
-		// SECURITY
-		// -------------------------------
-		case 0x90:
+			
+		/* -------------------------------*/
+		/* SECURITY 	                  */
+		/* -------------------------------*/
+		case 0x90u:
 			*_XXX_map = MEMORY_BLOCKED;
 			goto next_instruction;
-		case 0xA0:
+		case 0xA0u:
 			*_XXX_map = MEMORY_UNBLOCKED;
 			goto next_instruction;
-		case 0xC0:
-			if(*_XXX_map == MEMORY_UNBLOCKED)
+		case 0xC0u:
+			if (*_XXX_map == MEMORY_UNBLOCKED)
 				*_XXX_ ^= 0x42;
 			goto next_instruction;
 		default:
 			break;
 		}
-	
-		// -------------------------------
-		// SPECIAL INSTRUCTIONS
-		// -------------------------------
+
+		/* -------------------------------*/
+		/* SPECIAL INSTRUCTIONS           */
+		/* -------------------------------*/
 		switch (instruction)
 		{
-		case 0x1337: // print A
-			buf[0] = A;
-			buf[1] = '\0';
-			printf(buf);
+		case 0x1337: /* PRINT A */
+			putchar(A);
+			fflush( stdout );
 			goto next_instruction;
-		case 0xBEEF: // reset
+		case 0xBEEF: /* RESET */
 			PC = memory + 0x100;
 			A = 0x42;
 			goto jump;
-		case 0xEEEE: // NOP
+		case 0xEEEE: /* NOP */
 			goto next_instruction;
 		default:
 			break;
 		}
 
 	invalid_instruction:
-		A--; // quirk
+		A--;
 	next_instruction:
 		PC += 2;
 	jump:
-		//printf("\n");
-		continue;
+	if(PC >= memory + RAM_SIZE)
+		return 0;
+	continue;
 	}
-	
+
+	return 0;
 }
